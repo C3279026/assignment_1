a2738430f9a15cdc16939d7e2c71b7c28332f693
Connor  McDonald <c3279026@uon.edu.au>
2019-04-03 00:01:33 +0000

    assigning letter array

--- assesment_1.1..c
+++ assesment_1.1..c
@@ -2,7 +2,24 @@
 
 int main(void)
 {
-    printf("hello, world\n");
-    printf("my name is conor");
+    int count;
+    int k;
+    //int letters[] = {a, b, c, d, e, f, g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
+    //alphnos=alphabet numbers
+    int alphnos[26];// creating an array with 0-25 slots
+    //this for loop creates an array with numbers 0-25
+   
+
+
+//y=((x+k)%26) // y is the new letter and x is the non encyrpted letter
+
+
+
+    for (count=0;count<26;count++)
+    {
+        alphnos[count]='A' +count;// 'A' is an ascii +count moves that per loop thus assigning a capital letter
+        printf("%c\n",alphnos[count]); //%c means print the letter associated with at ascii
+
+    }
     return 0;
 }
