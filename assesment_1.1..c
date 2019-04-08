9aa235498945450af39da009c4f18298899a9e61
Connor  McDonald <c3279026@uon.edu.au>
2019-04-08 05:05:19 +0000

    pass  session

--- assesment_1.1..c
+++ assesment_1.1..c
@@ -1,30 +1,15 @@
 #include <stdio.h>
-
-int main(void)
-{
-    int count;
+void zeros(int*alphnos);
+int main(){
     int a;
-    int b;
-    int c;
-    int encrypt=2;
-    //alphnos=alphabet numbers
     int alphnos[26];
-    char message[]="HELLO";//enter message here
-    int newmessage[6];
-   //this for loop creates an array with numbers 0-25
-        for (count=0;count<26;count++)
-        {
-        // 'A' is an ascii +count moves that per loop thus assigning a capital letter starting at 0 
-        alphnos[count]='A' +count;
-        // finds new letter
-        b=alphnos[count]+encrypt;
-            if(b>90)
-            {
-            b=65+(count-(26-encrypt));// works for overflow
-            } 
-        }
-        
-        return 0;
+    char message[]"HELLO";
+    message[i]=message[i]-65;
+    printf("%d", message[i]);
 }
 
-//y=((x+k)%26) // y is the new letter and x is the non encyrpted letter
+void zero( int*alphnos) {
+int i; // Array index loop counter
+for(i = 0; i < N; i++)
+alphnos[i] = 0; // Use array syntax
+return; /
