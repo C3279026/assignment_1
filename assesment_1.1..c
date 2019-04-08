40cd4ff9c22803f54553e5c1d7c23814e0418067
Connor  McDonald <c3279026@uon.edu.au>
2019-04-08 02:15:39 +0000

    so close

--- assesment_1.1..c
+++ assesment_1.1..c
@@ -3,23 +3,30 @@
 int main(void)
 {
     int count;
+    int n=0;
     int b;
-    int encrypt=13;
-    //int message[]="type message in here";// message area
+    int encrypt=2;
     //alphnos=alphabet numbers
-    int alphnos[26];// creating an array with 0-25 slots
-    //this for loop creates an array with numbers 0-25
-   
-    for (count=0;count<26;count++)
-    {
-        alphnos[count]='A' +count;// 'A' is an ascii +count moves that per loop thus assigning a capital letter starting at 0
-        printf("%c",alphnos[count]); //%c means print the letter associated with at ascii
+    int alphnos[26];
+    char message[]="HELLO";//enter message here
+   //this for loop creates an array with numbers 0-25
+        for (count=0;count<26;count++)
+        {
+        // 'A' is an ascii +count moves that per loop thus assigning a capital letter starting at 0 
+        alphnos[count]='A' +count;
+        // finds new letter
         b=alphnos[count]+encrypt;
-        if(b>90){
-        b=65+(count-(26-encrypt));
+            if(b>90)
+            {
+            b=65+(count-(26-encrypt));// works for overflow
+            } 
         }
-        printf("%c\n", b);
-    }
-    
-    return 0;
-}//y=((x+k)%26) // y is the new letter and x is the non encyrpted letter
+        for(n=0; n<5;n++){
+        message[n]=b+message;
+        printf("%c\n", message);
+        }
+
+        return 0;
+}
+
+//y=((x+k)%26) // y is the new letter and x is the non encyrpted letter
