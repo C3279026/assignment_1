6a39d8a4ba3557e6c25c66e8d2188299d53f7ce5
Connor  McDonald <c3279026@uon.edu.au>
2019-04-08 01:01:07 +0000

    i got the rotation working

--- assesment_1.1..c
+++ assesment_1.1..c
@@ -3,23 +3,23 @@
 int main(void)
 {
     int count;
-    int k;
-    //int letters[] = {a, b, c, d, e, f, g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
+    int b;
+    int encrypt=13;
+    //int message[]="type message in here";// message area
     //alphnos=alphabet numbers
     int alphnos[26];// creating an array with 0-25 slots
     //this for loop creates an array with numbers 0-25
    
-
-
-//y=((x+k)%26) // y is the new letter and x is the non encyrpted letter
-
-
-
     for (count=0;count<26;count++)
     {
-        alphnos[count]='A' +count;// 'A' is an ascii +count moves that per loop thus assigning a capital letter
-        printf("%c\n",alphnos[count]); //%c means print the letter associated with at ascii
-
+        alphnos[count]='A' +count;// 'A' is an ascii +count moves that per loop thus assigning a capital letter starting at 0
+        printf("%c",alphnos[count]); //%c means print the letter associated with at ascii
+        b=alphnos[count]+encrypt;
+        if(b>90){
+        b=65+(count-(26-encrypt));
+        }
+        printf("%c\n", b);
     }
+    
     return 0;
-}
+}//y=((x+k)%26) // y is the new letter and x is the non encyrpted letter
