e8e1d7300999e8fafef742e00873eaacf7844824
Connor  McDonald <c3279026@uon.edu.au>
2019-04-10 00:29:03 +0000

    keep trying

--- assesment_1.1..c
+++ assesment_1.1..c
@@ -1,15 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 void zeros(int*alphnos);
 int main(){
     int a;
-    int alphnos[26];
-    char message[]"HELLO";
+    int b;
+    int i=0;
+    int encrypt=1;
+    char message[]="Z";
+    int N = ((sizeof(message)/ sizeof(message[0])));
+    printf("%d\n",N);
+    for(i=0;i<N-1;i++){
+    //printf("%d\n",message[i]);
     message[i]=message[i]-65;
-    printf("%d", message[i]);
+    //printf("%d\n", message[i]);
+    if(message[i]-encrypt<0){
+    message[i]= ((message[i] - encrypt)+26)% 26;
+    //printf("%d\n", message[i]);
+    message[i]= message[i]+65;
+     printf("%c,%d\n", message[i], message[i]);
+    }
+   else 
+    message[i]= ((message[i] - encrypt))% 26;
+   // printf("%d\n", message[i]);
+    message[i]= message[i]+65;
+     printf("%c,%d\n", message[i], message[i]);
+    }
+    return 0;
 }
-
-void zero( int*alphnos) {
-int i; // Array index loop counter
-for(i = 0; i < N; i++)
-alphnos[i] = 0; // Use array syntax
-return; /
